b37670d7040937e282395ffe176aaaa747052f47
CinkaFox <leofoxm40@gmail.com>
2026-09-11 22:23:55 +0300

    - fix: publish thinks

--- publish_update_resolver.sh
+++ publish_update_resolver.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 dotnet publish -c Release -r win-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true Nebula.UpdateResolver
 dotnet publish -c Release -r linux-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true Nebula.UpdateResolver
 
-mv ./publish/Nebula.UpdateResolver.exe ./publish/NebulaLauncher.exe
-mv ./publish/Nebula.UpdateResolver ./publish/NebulaLauncher
-mv ./publish/Nebula.UpdateResolver.pdb ./publish/NebulaLauncher.pdb
+mv ./publish/Nebula.UpdateResolver.exe ./publish/NebulaUpdateResolver.exe
+mv ./publish/Nebula.UpdateResolver ./publish/NebulaUpdateResolver
+mv ./publish/Nebula.UpdateResolver.pdb ./publish/NebulaUpdateResolver.pdb
