4953aadf26729f263c4058a76d48e54b5e0387ea
CinkaFox <leofoxm40@gmail.com>
2026-09-11 22:57:58 +0300

    - fix: self contain some launcher files

--- publish_launcher.sh
+++ publish_launcher.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "$0")"
 
 dotnet build -c Release 
-dotnet publish -c Release -r win-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true Nebula.Launcher
-dotnet publish -c Release -r linux-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true Nebula.Launcher
+dotnet publish -c Release -r win-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishSingleFile=true --self-contained true Nebula.Launcher
+dotnet publish -c Release -r linux-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishSingleFile=true --self-contained true Nebula.Launcher
 
 mv ./publish/Nebula.Launcher.exe ./publish/NebulaLauncher.exe
 mv ./publish/Nebula.Launcher ./publish/NebulaLauncher
